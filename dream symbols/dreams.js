dccdce90a8481e8aad6d90229b64350dc6fd73f5
H <hilary.dupont@gmail.com>
2018-04-26 12:36:07 -0400

    push
    push

---
+++ dream symbols/dreams.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+	$('#btn1').click(function(){
+		$('#video1').toggle();});
+
+	$('#btn2').click(function(){
+		$('#video2').toggle();});
+
+	$('#btn3').click(function(){
+		$('#video3').toggle();});
+
+	$('#btn4').click(function(){
+		$('#video4').toggle();});
+
+	$('#btn5').click(function(){
+		$('#video5').toggle();});
+
+	$('#btn6').click(function(){
+		$('#video6').toggle();});
+
+	$('#btn7').click(function(){
+		$('#song7').toggle();});
+
+	$('#btn8').click(function(){
+		$('#video8').toggle();});
+
+
+	$('#btn13').click(function(){
+		$('#video13').toggle();});
+
+	$('#btn10').click(function(){
+		$('#video10').toggle();});
+
+
+var beepOne = $("#fan")[0];
+$("#postit")
+	.mouseenter(function() {
+		beepOne.play();
+	});
+
+	var beepTwo = $("#gong")[0];
+$(".pin7")
+	.mouseenter(function() {
+		beepTwo.play();
+
+	});
+
+		var beepThree = $("#loon")[0];
+$(".pin8")
+	.mouseenter(function() {
+		beepThree.play();
+
+	});
+
+			var beepFour = $("#owl")[0];
+$(".pin2")
+	.mouseenter(function() {
+		beepFour.play();
+
+	});
+
+			var beepFive = $("#hyena")[0];
+$(".hyena")
+	.mouseenter(function() {
+		beepFive.play();
+
+	});
+
+				var beepSix = $("#button")[0];
+$(".jumpy")
+	.mouseenter(function() {
+		beepSix.play();
+
+	});
+
+					var beepSeven = $("#ice")[0];
+$(".pin15")
+	.mouseenter(function() {
+		beepSeven.play();
+
+	});
+	
+});
